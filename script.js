b06dbb1c6ab12970e0d79be7a6b3975a021568a1
Daniel Smit <danieldsmit@gmail.com>
2026-02-26 14:13:06 +0200

    updated navbar and spacing on mulitple pages

---
+++ script.js
@@ -0,0 +1,7 @@
+// nav - link dropdown - toggle show
+
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+
+dropdownToggle.addEventListener("mouseover", (event) => {
+    event.target.style.color = "blue";
+})
